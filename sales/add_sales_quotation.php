ba9f076bfa99f47b31ee3d4a4a0b64cf9e522b11
atararushi001 <atararushi111111@gmail.com>
2024-03-07 23:30:41 +0530

    editproduct

--- sales/add_sales_quotation.php
+++ sales/add_sales_quotation.php
@@ -25,6 +25,12 @@ if (isset($_GET['enquiry_id'])) {
             position: relative;
             vertical-align: middle;
         }
+
+        #productPopup {
+
+
+            display: none;
+        }
     </style>
 
     <script>
@@ -164,15 +170,15 @@ if (isset($_GET['enquiry_id'])) {
                 <div class="">
 
                     <table id="Producttable" class="min-w-full table-auto">
-                        <thead style="background-color: #F9FAFF; color:#031A61;">
+                        <thead style="background-color: #F9FAFF; ">
                             <tr>
                                 <th class="px-6 py-3 text-left text-sm leading-4 font-medium  tracking-wider">
-                                    <input type="checkbox" name="termheaderCheckbox" id="termheaderCheckbox"  >
+                                    <input type="checkbox" name="termheaderCheckbox" id="termheaderCheckbox">
                                 </th>
                                 <th class="px-6 py-3 text-left text-sm leading-4 font-medium  tracking-wider">
                                     #
                                 </th>
-                                <th class="px-6 py-3 text-left text-sm leading-4 font-medium  tracking-wider">
+                                <th class="px-6 py-3 text-left text-sm leading-4 font-medium  tracking-wider" colspan="2">
                                     Product Description
                                 </th>
                                 <th class="px-6 py-3 text-left text-sm leading-4 font-medium  tracking-wider">
@@ -202,7 +208,7 @@ if (isset($_GET['enquiry_id'])) {
                                 </th>
                             </tr>
                         </thead>
-                      
+
                     </table>
 
                 </div>
@@ -214,7 +220,7 @@ if (isset($_GET['enquiry_id'])) {
                 <div class="">
 
                     <table id="termtable" class="min-w-full table-auto">
-                        <thead style="background-color: #F9FAFF; color:#031A61;">
+                        <thead style="background-color: #F9FAFF; ">
                             <tr>
                                 <th class="px-6 py-3 text-left text-sm leading-4 font-medium  tracking-wider">
                                     <input type="checkbox" name="headerCheckbox" id="headerCheckbox">
@@ -237,19 +243,16 @@ if (isset($_GET['enquiry_id'])) {
                             $count++;
                         ?>
                             <tr>
-                                <td class="px-6 py-4 whitespace-no-wrap"><input type="checkbox" name="terms_condition[]" value="<?php echo $terms_conditiondata['Terms_Condition_description']; ?>"></td>
+                                <td class="px-6 py-4 whitespace-no-wrap"><input type="checkbox" name="terms_condition[]" value="<?php echo $terms_conditiondata['Terms_Condition_id']; ?>"></td>
                                 <td class="px-6 py-4 whitespace-no-wrap"><?php echo $count; ?></td>
                                 <td class="px-6 py-4 whitespace-no-wrap"><?php echo $terms_conditiondata['Terms_Condition_description']; ?></td>
                                 <td class="px-6 py-4 whitespace-no-wrap"><?php echo $terms_conditiondata['Terms_Condition_Condition']; ?></td>
                             </tr>
                         <?php } ?>
                     </table>
-
-      
-                  
-
                 </div>
             </div>
+
             <div class="flex flex-wrap gap-5">
                 <button class="text-white text-sm px-4 py-2 w-28" style="background-color: #007bff;" name="add_sales_quotation">Save</button>
                 <button class="text-white text-sm px-4 py-2" style="background-color: #007bff;">Save & Preview</button>
@@ -258,21 +261,90 @@ if (isset($_GET['enquiry_id'])) {
             </div>
         </div>
     </form>
+    <div class="fixed inset-0 items-center justify-center bg-black bg-opacity-50 p-4 transition-all duration-300" style="z-index: 99;" id="productPopup" style="display: BLOCK;">
+        <div id="bw_popup" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-sm shadow-lg" style="width:1100px;">
+            <div class="flex justify-between border-b">
+                <h2 class="text-gray-800 font-semibold p-4 text-xl">Terms Condition</h2>
+                <svg id="closeCCategory" onclick="closeModal('terms_conditionPopup')" class="cursor-pointer mt-3 mr-2 close-button" width="35" height="35" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M37.5 12.5L12.5 37.5" stroke="black" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <path d="M12.5 12.5L37.5 37.5" stroke="black" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+            </div>
+            <form class="p-4" name="terms_conditionform" action="../include/function.php" id="terms_conditionform">
 
+                <div class="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 p-4 gap-4">
+                    <div>
+                        <label for="customer_type" class="text-gray-700 font-semibold">Product Description</label>
+                        <input type="text" name="Product_Description" id="Product_Description" placeholder="Product Description" class="border rounded-sm outline-none p-2 w-full focus:ring focus:ring-blue-400 mt-2">
+                    </div>
+                    <div>
+                        <label for="customer_type" class="text-gray-700 font-semibold">Part Number</label>
+                        <input type="text" name="part_number" id="part_number" placeholder="part number" class="border rounded-sm outline-none p-2 w-full focus:ring focus:ring-blue-400 mt-2">
+                    </div>
+
+                    <input type="hidden" name="quotation_Product_id" class="border rounded-sm outline-none p-2 w-full focus:ring focus:ring-blue-400 mt-2">
+                </div>
+                <div class="grid lg:grid-cols-4 md:grid-cols-1 sm:grid-cols-1 p-4 gap-4">
+                    <div>
+                        <label for="customer_type" class="text-gray-700 font-semibold">HSN Code</label>
+                        <input type="text" name="hsn_code" id="hsn_code" placeholder="HSN Code" class="border rounded-sm outline-none p-2 w-full focus:ring focus:ring-blue-400 mt-2">
+                    </div>
+                    <div>
+                        <label for="customer_type" class="text-gray-700 font-semibold">Part Number</label>
+                        <input type="text" name="part_number" id="part_number" placeholder="Terms Condition Condition" class="border rounded-sm outline-none p-2 w-full focus:ring focus:ring-blue-400 mt-2">
+                    </div>
+                    <div>
+                        <label for="customer_type" class="text-gray-700 font-semibold">Part Number</label>
+                        <input type="text" name="" id="Terms_Condition_Condition" placeholder="Terms Condition Condition" class="border rounded-sm outline-none p-2 w-full focus:ring focus:ring-blue-400 mt-2">
+                    </div>
+                    <div>
+                        <label for="customer_type" class="text-gray-700 font-semibold">Part Number</label>
+                        <input type="text" name="" id="Terms_Condition_Condition" placeholder="Terms Condition Condition" class="border rounded-sm outline-none p-2 w-full focus:ring focus:ring-blue-400 mt-2">
+                    </div>
+
+                    <input type="hidden" name="Product_id" class="border rounded-sm outline-none p-2 w-full focus:ring focus:ring-blue-400 mt-2">
+                </div>
+                <div class="grid lg:grid-cols-4 md:grid-cols-1 sm:grid-cols-1 p-4 gap-4">
+                    <div>
+                        <label for="customer_type" class="text-gray-700 font-semibold">CGST</label>
+                        <input type="text" name="" id="Terms_Condition_description" placeholder="Terms_Condition_description" class="border rounded-sm outline-none p-2 w-full focus:ring focus:ring-blue-400 mt-2">
+                    </div>
+                    <div>
+                        <label for="customer_type" class="text-gray-700 font-semibold">SGST</label>
+                        <input type="text" name="" id="Terms_Condition_Condition" placeholder="Terms Condition Condition" class="border rounded-sm outline-none p-2 w-full focus:ring focus:ring-blue-400 mt-2">
+                    </div>
+                    <div>
+                        <label for="customer_type" class="text-gray-700 font-semibold">IGST</label>
+                        <input type="text" name="" id="Terms_Condition_Condition" placeholder="Terms Condition Condition" class="border rounded-sm outline-none p-2 w-full focus:ring focus:ring-blue-400 mt-2">
+                    </div>
+                    <div>
+                        <label for="customer_type" class="text-gray-700 font-semibold">Tax Amount</label>
+                        <input type="text" name="" id="Terms_Condition_Condition" placeholder="Terms Condition Condition" class="border rounded-sm outline-none p-2 w-full focus:ring focus:ring-blue-400 mt-2">
+                    </div>
+
+                    <input type="hidden" name="Product_id" class="border rounded-sm outline-none p-2 w-full focus:ring focus:ring-blue-400 mt-2">
+                </div>
+                <div class="flex items-center justify-start gap-4 mt-10 ">
+                    <button class="text-white text-sm px-4 py-2 w-28" onclick="savedatanow('terms_conditionform','<?php echo  isset($_GET['Terms_Condition_id']) ?  'terms_condition_update' : 'Terms_Condition_Condition' ?>')" type="button" id="openButton" style="background-color: #007bff;">Save</button>
+                    <button type="button" class="border bg-white text-sm px-4 py-2 w-28" onclick="closeModal('terms_conditionPopup')" style="color: #007bff; border: 1px solid #007bff;">Cancel</button>
+
+            </form>
+        </div>
+    </div>
     <script src="../assets/js/script.js"></script>
     <script>
-                        $(document).ready(function() {
-           
-                          
-                            $('#headerCheckbox').click(function() {
-                                $('input[name="terms_condition[]"]').prop('checked', this.checked);
-                            });
+        $(document).ready(function() {
 
-                            $('#termheaderCheckbox').click(function() {
-                                $('input[name="Productheadercheckbox[]"]').prop('checked', this.checked);
-                            });
-                        });
-                    </script>
+
+            $('#headerCheckbox').click(function() {
+                $('input[name="terms_condition[]"]').prop('checked', this.checked);
+            });
+
+            $('#termheaderCheckbox').click(function() {
+                $('input[name="Productheadercheckbox[]"]').prop('checked', this.checked);
+            });
+        });
+    </script>
     <script>
         function getenquirydata(e) {
             // var data = e.params.data;
@@ -284,7 +356,7 @@ if (isset($_GET['enquiry_id'])) {
                     Enquirydata: e
                 },
                 success: function(response) {
-                    console.log(response);
+                    // console.log(response);
 
                     let data = JSON.parse(response);
                     let select = document.getElementById('sales_quotation_enquiry');
@@ -292,9 +364,9 @@ if (isset($_GET['enquiry_id'])) {
                     // Empty the select element
                     select.innerHTML = '';
                     let option = document.createElement('option');
-                        option.value = "";
-                        option.text = "Select Enquiry";
-                        select.appendChild(option);
+                    option.value = "";
+                    option.text = "Select Enquiry";
+                    select.appendChild(option);
                     data.forEach(item => {
                         let option = document.createElement('option');
                         option.value = item.enquiry_id;
@@ -343,18 +415,25 @@ if (isset($_GET['enquiry_id'])) {
                         let taxAmount = amount * taxRate;
 
                         row.innerHTML = `
-        <td class="px-6 py-4 whitespace-no-wrap"><input type="checkbox" name="Productheadercheckbox[]"></td>
-        <td class="px-6 py-4 whitespace-no-wrap">${index + 1}</td>
-        <td class="px-6 py-4 whitespace-no-wrap">${item.enquiry_p_product_description}</td>
-        <td   class="px-6 py-4 whitespace-no-wrap" >${item.enquiry_p_part_number}</td>
-        <td   class="px-6 py-4 whitespace-no-wrap" >${item.enquiry_p_product_hsn_code}</td>
-        <td   class="px-6 py-4 whitespace-no-wrap" >${item.enquiry_p_product_quantity}</td>
-        <td   class="px-6 py-4 whitespace-no-wrap" >${item.enquiry_p_product_rate}</td>
-        <td   class="px-6 py-4 whitespace-no-wrap" >${item.enquiry_p_product_amount}</td>
-        <td   class="px-6 py-4 whitespace-no-wrap" >00</td>
-        <td   class="px-6 py-4 whitespace-no-wrap" >00</td>
-        <td   class="px-6 py-4 whitespace-no-wrap" >Action</td>
-    `;
+<td class="px-6 py-4 whitespace-no-wrap" ><input type="checkbox" name="Productheadercheckbox[]"></td>
+<td class="px-6 py-4 whitespace-no-wrap">${index + 1}</td>
+<td class="px-6 py-4 whitespace-no-wrap" colspan="2">${item.enquiry_p_product_description}</td>
+<td   class="px-6 py-4 whitespace-no-wrap" >${item.enquiry_p_part_number}</td>
+<td   class="px-6 py-4 whitespace-no-wrap" >${item.enquiry_p_product_hsn_code}</td>
+<td   class="px-6 py-4 whitespace-no-wrap" >${item.enquiry_p_product_quantity}</td>
+<td   class="px-6 py-4 whitespace-no-wrap" >${item.enquiry_p_product_rate}</td>
+<td   class="px-6 py-4 whitespace-no-wrap" >${item.enquiry_p_product_amount}</td>
+<td   class="px-6 py-4 whitespace-no-wrap" >00</td>
+<td   class="px-6 py-4 whitespace-no-wrap" >00</td>
+<td   class="px-6 py-4 whitespace-no-wrap" >
+<button type="button" onclick="editproduct(${item.enquiry_p_id })">
+<svg class="mt-2" width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12 20H21" stroke="#8A8A8A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+        <path d="M16.5 3.50023C16.8978 3.1024 17.4374 2.87891 18 2.87891C18.2786 2.87891 18.5544 2.93378 18.8118 3.04038C19.0692 3.14699 19.303 3.30324 19.5 3.50023C19.697 3.69721 19.8532 3.93106 19.9598 4.18843C20.0665 4.4458 20.1213 4.72165 20.1213 5.00023C20.1213 5.2788 20.0665 5.55465 19.9598 5.81202C19.8532 6.06939 19.697 6.30324 19.5 6.50023L7 19.0002L3 20.0002L4 16.0002L16.5 3.50023Z" stroke="#8A8A8A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+      </svg>
+</button>
+</td>
+`;
 
                         totalQuantity += quantity;
                         totalAmount += amount;
@@ -370,9 +449,9 @@ if (isset($_GET['enquiry_id'])) {
 <td colspan="2" class="px-6 py-4 whitespace-no-wrap">
 Amount In Words:
 </td>
-    <td colspan="9" class="px-6 py-4 whitespace-no-wrap">
-        Amount In Words: INR ${netAmount.toFixed(2)} only Total Quantity NOs. ${totalQuantity} Total ${totalAmount.toFixed(2)} Tax Amount ${totalTaxAmount.toFixed(2)} Net Amount (INR) ${netAmount.toFixed(2)}
-    </td>
+<td colspan="9" class="px-6 py-4 whitespace-no-wrap">
+Amount In Words: INR ${netAmount.toFixed(2)} only Total Quantity NOs. ${totalQuantity} Total ${totalAmount.toFixed(2)} Tax Amount ${totalTaxAmount.toFixed(2)} Net Amount (INR) ${netAmount.toFixed(2)}
+</td>
 `;
                 },
                 error: function(error) {
@@ -399,7 +478,7 @@ Amount In Words:
                     $('#sales_quotation_currency').trigger('change');
                     let sales_quotation_sr_by = document.getElementById('sales_quotation_sr_by');
                     sales_quotation_sr_by.value = data[0].enquiry_source;
-                    sales_quotation_sr_by.disabled = true; 
+                    sales_quotation_sr_by.disabled = true;
                 },
                 error: function(error) {
                     console.error('AJAX request failed: ' + error);
@@ -407,6 +486,14 @@ Amount In Words:
             });
 
 
+        }
+
+        function editproduct(id) {
+            console.log(id);
+
+            document.getElementById('productPopup').style.display = 'block';
+
+            document.querySelector('input[name="quotation_Product_id"]').value = id;
         }
         $(document).ready(function() {
 
